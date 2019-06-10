1d5cf6900bc635625e2343090a048a24d90553e8
Ahmed Hafez <Ahmed41901@gmail.com>
2019-06-10 20:10:17 +0200

    BinarySearch algorithm was added

---
+++ C#/AlgorithmsDesign/Algorithms/SearchingAlgorithms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static class SearchingAlgorithms
+    {
+        /// <summary>
+        /// Searching for the <paramref name="value"/> provided in the provided <see cref="IList{T}"/> object
+        /// </summary>
+        /// <typeparam name="T">The type of the objects in the <see cref="IList{T}"/></typeparam>
+        /// <param name="container">The container to search in</param>
+        /// <param name="value">The value to search for</param>
+        public static bool BinarySearch<T>(IList<T> container, T value) 
+            where T : IComparable
+        {
+            int lowPtr = 0;
+            int highPtr = container.Count;
+
+            while (lowPtr < highPtr)
+            {
+                int midPtr = (lowPtr + highPtr) / 2;
+                int comparingValue = container.ElementAt<T>(midPtr).CompareTo(value);
+
+                if (comparingValue > 0)
+                    highPtr = midPtr - 1;
+                else if (comparingValue < 0)
+                    lowPtr = midPtr + 1;
+                else return true;
+            }
+
+            // Check the state when (lowPtr == highPtr)
+            if (container.ElementAt<T>(lowPtr).CompareTo(value) == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ C#/AlgorithmsDesign/AlgorithmsDesign/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Algorithms;
+
+namespace AlgorithmsDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 8, 11, 20 };
+            Console.WriteLine(SearchingAlgorithms.BinarySearch<int>(arr, 20));
+
+            List<string> list = new List<string>() { "Ahmed", "ahmed", "sara" };
+            Console.WriteLine(SearchingAlgorithms.BinarySearch<string>(list, "ahmed"));
+        }
+    }
+}
