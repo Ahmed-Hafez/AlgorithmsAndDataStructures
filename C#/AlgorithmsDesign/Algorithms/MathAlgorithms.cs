ee9de9634c2ee232ed50a2ae6acf0243059d65d5
Ahmed Hafez <Ahmed41901@gmail.com>
2019-06-12 23:45:07 +0200

    Math Algorithms was added
    Grade school algoritm and Karatsuba multiplication algorithm were added

---
+++ C#/AlgorithmsDesign/Algorithms/MathAlgorithms.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class MathAlgorithms
+    {
+        /// <summary>
+        /// Implementaion of grade school algorithm
+        /// </summary>
+        /// <param name="firstNum">First number</param>
+        /// <param name="secondNum">Second number</param>
+        public static long GradeSchoolMultiplication(long firstNum, long secondNum)
+        {
+            string S_SecondNum = secondNum.ToString();
+
+            long sum = 0;
+
+            for (int i = S_SecondNum.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                sum += (firstNum * (S_SecondNum[i] - '0')) * Convert.ToInt64(Math.Pow(10, j));
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Implementaion of Karatsuba multiplication algorithm
+        /// </summary>
+        /// <param name="firstNum">First number</param>
+        /// <param name="secondNum">Second number</param>
+        public static long KaratsubaMultiplication(long firstNum, long secondNum)
+        {
+            // Find the min size of two integers
+            int n = Math.Min(firstNum.ToString().Length, secondNum.ToString().Length);
+
+            if (n == 1)
+                return firstNum * secondNum;
+
+            n = n / 2;
+
+            //The power for calculating a,b,c,d
+            long p = (long)Math.Pow(10, n);
+
+            long a = firstNum / p;
+            long b = firstNum % p;
+            long c = secondNum / p;
+            long d = secondNum % p;
+
+            // Performing Karatsuba equation => ac * 10^n + (10^((n+1)/2)) * (ad + bc) + bd
+            long ac = KaratsubaMultiplication(a, c);
+            long bd = KaratsubaMultiplication(b, d);
+            long f = KaratsubaMultiplication(a + b, c + d);
+            return ((long)Math.Pow(10, 2 * n) * ac) + bd + ((f - bd - ac) * p);
+        }
+    }
+}
--- C#/AlgorithmsDesign/AlgorithmsDesign/Program.cs
+++ C#/AlgorithmsDesign/AlgorithmsDesign/Program.cs
@@ -6,8 +6,46 @@ namespace AlgorithmsDesign
 {
     class Program
     {
+        /*public static long multiply(long x, long y)
+        {
+            long result = 0;
+
+            int size1 = GetSize(x);
+            int size2 = GetSize(y);
+
+            //find the max size of two integers
+            int N = Math.Max(size1, size2);
+
+            if (N < 2)
+                return x * y;
+
+            //Max length divided by two and rounded up
+            N = (N / 2) + (N % 2);
+
+            //The mulitplying factor for calculating a,b,c,d
+            long m = (long)Math.Pow(10, N);
+
+            long b = x % m;
+            long a = x / m;
+            long c = y / m;
+            long d = y % m;
+
+            long z0 = multiply(a, c);
+            long z1 = multiply(b, d);
+            long z2 = multiply(a + b, c + d);
+
+            result = ((long)Math.Pow(10, N * 2) * z0) + z1 + ((z2 - z1 - z0) * m);
+            return result;
+        }*/
         static void Main(string[] args)
         {
+            Console.WriteLine("G = {0},\nK = {1},\nP = {2},\nX = ",
+                MathAlgorithms.GradeSchoolMultiplication(999999999, 999999999),
+                MathAlgorithms.KaratsubaMultiplication(999999999, 999999999),
+                999999999L * 999999999L);
+
+            //Console.WriteLine(Math.Pow(10, 2).ToString());
+
             int[] arr = { 1, 2, 3, 4, 5, 8, 11, 20, 4,465,4645,645,675,765,7,567,65 };
             Console.WriteLine(SearchingAlgorithms.BinarySearch<int>(arr, 20));
 
