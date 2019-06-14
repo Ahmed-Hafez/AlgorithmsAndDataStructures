4f6704183dfb0e22eb88675f10b6e0eb748e91e3
Ahmed-Hafez <ahmed41901@gmail.com>
2019-06-14 14:57:25 +0200

    Merge sort was added and functions have became generics

--- Java/AlgorithmsDesign/src/Algorithms/SortingAlgorithms.java
+++ Java/AlgorithmsDesign/src/Algorithms/SortingAlgorithms.java
@@ -1,18 +1,94 @@
 package Algorithms;
 
-public class SortingAlgorithms {
+import DataStructures.Queue;
+
+import java.util.List;
+
+public class SortingAlgorithms{
 
     // Implementation of selection sort algorithm
-    public static void selectionSort(int[] list){
+    public static <T extends Comparable> void SelectionSort (T[] container){
 
-        for (int i = 0; i < list.length; i++){
-            for(int j = i; j < list.length; j++){
-                if(list[i] > list[j]){
-                    int temp = list[i];
-                    list[i] = list[j];
-                    list[j] = temp;
+        for (int i = 0; i < container.length; i++)
+        {
+            for (int j = i; j < container.length; j++)
+            {
+                if (container[i].compareTo(container[j]) > 0)
+                {
+                    T temp = container[i];
+                    container[i] = container[j];
+                    container[j] = temp;
                 }
             }
+        }
+    }
+
+
+    // Implementation of selection sort algorithm
+    public static <T extends Comparable> void MergeSort(T[] container, int low, int high)
+    {
+        if (low < high)
+        {
+            int mid = (low + high) / 2;
+            MergeSort(container, low, mid);
+            MergeSort(container, mid + 1, high);
+            Merge(container, low, high);
+        }
+    }
+
+
+    private static <T extends Comparable> void Merge(T[] container, int low, int high)
+    {
+        // The middle of the part which we sort in the container
+        int mid = (low + high) / 2;
+
+        // Containers used in merge sort
+        // Note : This Queue data structure is implemented by me,
+        //        you can see it in the C#/DataStructuresDesign folder on the repo.
+        Queue<T> q1 = new Queue<>();
+        Queue<T> q2 = new Queue<>();
+
+        int i = low; //Counter
+
+        // Inserting the first half part in the specified
+        // part in the container in a queue
+        for (; i <= mid; i++)
+            q1.push(container[i]);
+
+        // Inserting the first half part in the specified
+        // part in the container in a queue
+        for (; i <= high; i++)
+            q2.push(container[i]);
+
+        i = low;
+
+        // Compare the items in the two queues
+        while (!q1.isEmpty() && !q2.isEmpty())
+        {
+            if (q1.peek().compareTo(q2.peek()) < 0)
+            {
+                container[i++] = q1.peek();
+                q1.pop();
+            }
+            else
+            {
+                container[i++] = q2.peek();
+                q2.pop();
+            }
+        }
+
+        // Check if the first queue still has items
+        while (!q1.isEmpty())
+        {
+            container[i++] = q1.peek();
+            q1.pop();
+        }
+
+        // Check if the second queue still has items
+        while (!q2.isEmpty())
+        {
+            container[i++] = q2.peek();
+            q2.pop();
         }
     }
 
--- Java/AlgorithmsDesign/src/AlgorithmsDesign/Main.java
+++ Java/AlgorithmsDesign/src/AlgorithmsDesign/Main.java
@@ -2,6 +2,7 @@ package AlgorithmsDesign;
 
 import Algorithms.MathAlgorithms;
 import Algorithms.SearchingAlgorithms;
+import Algorithms.SortingAlgorithms;
 
 public class Main {
 
@@ -13,6 +14,20 @@ public class Main {
         System.out.println(SearchingAlgorithms.binarySearch(new int[]{1,2,3,4,5,6}, 3));
 
 
+        Integer[] arr = {1,4,2,3,6,7,76,324,12,12,43,456,987};
+        Integer[] arr2 = {7, 4, 2, 6, 1, 8, 3};
+
+        SortingAlgorithms.MergeSort(arr, 0, arr.length-1);
+        SortingAlgorithms.MergeSort(arr2, 0, arr2.length-1);
+
+        for (int item : arr) {
+            System.out.print(item + " ");
+        }
+
+        System.out.println();
+        for (int item : arr2) {
+            System.out.print(item + " ");
+        }
 
     }
 }
