1540647f58f6c800919a6fb6a655441592bb9085
Ahmed-Hafez <ahmed41901@gmail.com>
2019-06-20 01:01:27 +0200

    Sorted linked list was added

--- Python/DataStructuresDesign/venv/LinkedList.py
+++ Python/DataStructuresDesign/venv/LinkedList.py
@@ -51,7 +51,7 @@ class LinkedList:
 
         return current
 
-    def display_list(self):
+    def __str__(self):
         list = ""
 
         current = self.__first
@@ -60,4 +60,74 @@ class LinkedList:
             list = list + str(current) + " "
             current = current.next
 
+        return list
+
+
+class SortedLinkedList:
+
+    def __init__(self):
+        self.__first = None
+
+    def insert(self, value):
+        link = Link(value)
+
+        current = self.__first
+        previous = None
+
+        while current is not None and current.value < value:
+            previous = current
+            current = current.next
+            pass
+
+        if previous is None:  # In the beginning
+            self.__first = link
+            pass
+
+        else:
+            previous.next = link
+            pass
+
+        link.next = current
+
+    def remove(self, value):
+
+        if self.__first is None:
+            raise Exception("Ops, The list is empty")
+
+        current = self.__first
+        previous = self.__first
+
+        while current.value is not value:
+            if current.next is None:
+                raise Exception("Item not found")
+
+            previous = current
+            current = current.next
+
+        if current is self.__first:
+            self.__first = self.__first.next
+            pass
+        else:
+            previous.next = current.next
+
+    def find(self, value):
+        current = self.__first
+
+        while current.value is not value:
+            if current.next is None:
+                return None
+
+            current = current.next
+            pass
+
+        return current
+
+    def __str__(self):
+        list = ""
+
+        current = self.__first
+
+        while current is not None:
+            list = list + str(current) + " "
+            current = current.next
         return list
